6ad1d3d0de5a8eebe9bcac41e66174eb9d4917d9
JaCkIsO <jackisodev@gmail.com>
2021-05-13 18:18:15 -0500

    Update App.js
    Refactor Central Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/home/index'
-import Login from './components/login/Login'
-import Signup from './components/Signup/Signup'
+import Admin from './pages/admin';
+import Seller from './pages/seller';
+import Buyer from './pages/buyer';
+import SellerPage from './pages/common/seller-page';
+import ProductPage from './pages/common/product-page';
+import Login from './pages/common/login';
+import Register from './pages/common/register';
 function App() {
 
   return (
@@ -10,12 +15,14 @@ function App() {
 
       <Switch>
         <Route exact path="/login" component={Login} />
-
-        <Route exact path="/signup" component={Signup} />
-
-        {/* <Route path="/products" component={Products} /> */}
-        <Route path="/" component={Home} />
-
+        <Route exact path="/register" component={Register} />    
+        <Route path="/admin" component={Admin}/>
+        <Route path="/seller" component={Seller}/>
+        <Route path="/buyer" component={Buyer}/>
+        <Route path="/sellers/:id" component={SellerPage}/>
+        <Route path="/products/:id" component={ProductPage}/>
+        <Route exact path="/" component={Home} />
+        <Route><h1>404 Not Found</h1></Route>
       </Switch>
 
     </BrowserRouter>
